709faa8fe387865faf75f0fcb02c314e9052e76b
Robert Kirui <kirugik79@gmail.com>
2022-04-17 19:56:09 +0300

    Fix: Fix landing page and search profile page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,9 @@ import { UserProfileDataComponent } from './components/user-profile-data/user-pr
 import { UserProfileDetailsComponent } from './components/user-profile-details/user-profile-details.component';
 import { UserReposComponent } from './components/user-repos/user-repos.component';
 import { HttpClientModule } from '@angular/common/http';
+import { MyProfileComponent } from './components/my-profile/my-profile.component';
+import { GithubuserProfileComponent } from './components/githubuser-profile/githubuser-profile.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +21,10 @@ import { HttpClientModule } from '@angular/common/http';
     UserProfileComponent,
     UserProfileDataComponent,
     UserProfileDetailsComponent,
-    UserReposComponent
+    UserReposComponent,
+    MyProfileComponent,
+    GithubuserProfileComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/githubuser-profile/githubuser-profile.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-githubuser-profile',
+  templateUrl: './githubuser-profile.component.html',
+  styleUrls: ['./githubuser-profile.component.css']
+})
+export class GithubuserProfileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
