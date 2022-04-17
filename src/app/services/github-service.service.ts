02ff9eb110f02bb103f0f772c8416e09c66c2d68
Robert Kirui <kirugik79@gmail.com>
2022-04-17 13:13:12 +0300

    Feat: Display user repos using Http request

--- src/app/services/github-service.service.ts
+++ src/app/services/github-service.service.ts
@@ -17,4 +17,14 @@ export class GithubServiceService {
     catchError(this.handleErrors)
     );
   }
+
+
+  // display github repos
+  public getRepos (searchTerm:any):Observable<any[]> {
+    let dataURL = `https://api.github.com/users/${searchTerm}/repos?client_id=${client_id}&client_secret=${client_secret}`;
+    return this.httpClient.get<any[]>(dataURL).pipe(
+      // retry(count:1),
+    catchError(this.handleErrors)
+    );
+  }
 }
