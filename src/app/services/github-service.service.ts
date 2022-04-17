0d3bdf1c583a30609a135b7c6bb22e5e8316e361
Robert Kirui <kirugik79@gmail.com>
2022-04-17 13:12:08 +0300

    Function to display user profile

--- src/app/services/github-service.service.ts
+++ src/app/services/github-service.service.ts
@@ -7,4 +7,14 @@ import { HttpClient } from '@angular/common/http';
 export class GithubServiceService {
 
   constructor(private httpClient:HttpClient) { }
+
+
+  // display github profile
+  public getProfile (searchTerm:any):Observable<any> {
+    let dataURL = `https://api.github.com/users/${searchTerm}?client_id=${client_id}&client_secret=${client_secret}`;
+    return this.httpClient.get<any>(dataURL).pipe(
+      // retry(count:1),
+    catchError(this.handleErrors)
+    );
+  }
 }
