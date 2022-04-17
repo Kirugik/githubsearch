60f024295e0c554bcd28a3dcd86071f6b816ba05
Robert Kirui <kirugik79@gmail.com>
2022-04-17 13:05:02 +0300

    Feat: structure the user details section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { SearchComponent } from './components/search/search.component';
 import { UserProfileComponent } from './components/user-profile/user-profile.component';
 import { UserProfileDataComponent } from './components/user-profile-data/user-profile-data.component';
+import { UserProfileDetailsComponent } from './components/user-profile-details/user-profile-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SearchComponent,
     UserProfileComponent,
-    UserProfileDataComponent
+    UserProfileDataComponent,
+    UserProfileDetailsComponent
   ],
   imports: [
     BrowserModule,
